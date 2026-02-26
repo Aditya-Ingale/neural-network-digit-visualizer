755ce9cebafcaffdf62fa34c390cc74f53ad2424
Aditya-Ingale <adityaa.ingale@gmail.com>
2026-02-26 22:09:14 +0530

    feat: add MNIST training pipeline and saved model

---
+++ backend/train/train_model.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+import os
+
+def load_data():
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    return (x_train, y_train), (x_test, y_test)
+
+def preprocess_data(x_train, y_train, x_test, y_test):
+    #Normalize
+    x_train = x_train.astype("float32") / 255.0
+    x_test = x_test.astype("float32") / 255.0
+
+    return x_train, y_train, x_test, y_test
+
+def build_model():
+    inputs = keras.Input(shape=(28, 28))
+    x = layers.Flatten()(inputs)
+    x = layers.Dense(128, activation="relu")(x)
+    x = layers.Dense(64, activation="relu")(x)
+    outputs = layers.Dense(10, activation="softmax")(x)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    model.compile(
+        optimizer="adam", 
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+def train_model(model, x_train, y_train):
+    model.fit(
+        x_train,
+        y_train,
+        epochs=5,
+        batch_size=128,
+        validation_split=0.1
+    )
+
+def save_model(model):
+    model_dir = os.path.join(os.path.dirname(__file__), "..", "app", "model")
+    os.makedirs(model_dir, exist_ok=True)
+
+    model_path = os.path.join(model_dir, "mnist_model.keras")
+    model.save(model_path)
+
+    print(f"Model saved at: {model_path}")
+
+def main():
+    (x_train, y_train), (x_test, y_test) = load_data()
+    x_train, y_train, x_test, y_test = preprocess_data(x_train, y_train, x_test, y_test)
+
+    model = build_model()
+    train_model(model, x_train, y_train)
+
+    save_model(model)
+
+if __name__ == "__main__":
+    main()
